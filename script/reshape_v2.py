86817065bccfbe1578f01542b6c1016fc26a2f3f
yuying <xieyuyying993@gmail.com>
2025-11-06 14:32:08 -0800

    update version two data cleanning script

--- script/reshape_v2.py
+++ script/reshape_v2.py
@@ -27,82 +27,68 @@ def load_geojson_to_data(inpath):
         data = json.load(f)
     return data
 
-#计算一个ring最小/大点距
-def cal_minmax_dist(ring):
-    distance = []
-    for i in range(len(ring)-1):
-        p1,p2=ring[i], ring[i+1]
-        dist = math.dist(p1,p2)
-        distance.append(dist)
-    return max(distance)
-
 ##找到触碰边界的点。
-#传入一个feature的all coord(很多polygon)
-#return split字典：哪个polygon 第几个值变换
-def find_breakpoint(all_coord,offshore):
-    split={}
-    for i, p in enumerate(all_coord): #循环for 每一个polygon
-        #提取每一个polygon的ring(如果有hole的话也只提取第一个ring)
-        ring = p[0]
-        #检查是不是有hole 此处先不处理hole
-        if len(p) >1:
-            print(f"Number{i} polygon has a hole")
-        #检查是不是在边界外
-        #每次储存
-        split_point =[]
-        for s, coord in enumerate(ring):
-            x,y = coord
-            if not (extreme_smaller["xmin"] <= x <= extreme_smaller["xmax"] and 
-                extreme_smaller["ymin"] <= y <= extreme_smaller["ymax"]):
-                split_point.append([s, coord])
-            else:
-                continue
-        #print(split_point)
-        if split_point:
-            split[i]=split_point
-        elif not split_point:
+def find_breakpoint(ring,offshore):
+    """
+    Find point touch the extreme boundarys
+    
+    :param all_coord: geometory of a ring
+    :param offshour: adjust the size of extreme from startard
+    :return: split dic, which polygon, which point.
+    """
+    split_point =[]
+    for s, coord in enumerate(ring):
+        x,y = coord
+        if not (extreme_smaller["xmin"] <= x <= extreme_smaller["xmax"] and 
+            extreme_smaller["ymin"] <= y <= extreme_smaller["ymax"]):
+            split_point.append([s, coord])
+        else:
             continue
-    print(f"--------all touch boundary points in this data with offshore {offshore}----------")
-    print(split)
-    return split
+    return split_point
 
-#找极端值 
-#传入一个feature的all coord(很多polygon)
-#return split字典：哪个polygon 第几个值变换
-def find_outliner(all_coord):
-    outliner={}
-    max_list=[]
-    for i, p in enumerate(all_coord): #循环for 每一个polygon
-        #提取每一个polygon的ring(如果有hole的话也只提取第一个ring)
-        ring = p[0]
-        #检查是不是有hole 此处先不处理hole
-        if len(p) >1:
-            print(f"Number{i} polygon has a hole")
-        #检查是不是坐标轴变化
-        #每次储存
-        max = cal_minmax_dist(ring)
-        max_list.append(max)
-    print (sorted(max_list))
-        
 
-## 主程序
+##找关于质心极角差大于90度的点
+def find_crosspoint(ring):
+    pts = np.array(ring)
+    cx,cy = pts.mean(axis = 0)
+    dx = pts[:,0] - cx
+    dy = pts[:,1] - cy
+    angles = np.arctan2(dy,dx)
+    
+    # detect sudden angle jump > pi/2
+    for i in range (len(angles)-1):
+        if angles[i+1] - angles[i] > np.pi/2:
+            print (f"Jump at index {i}: {angles[i]} -> {angles[i+1]}")
+
+##重组polygon
+def reconstruct_polygon(ring):
+    pass
+
+## main app
 def main(): 
-    inpath = "/Users/xy/Documents/workspace/widget-spilhaus/data/rawChina54099.geojson"
+    inpath = "/Users/xy/Documents/workspace/SpilhausPolygon/data/rawChina54099.geojson"
     data= load_geojson_to_data(inpath)
     if data.get("type") == "FeatureCollection":
         all_data = data.get("features")
         print(f"整个数据结构有{len(all_data)}feature")
         for f,dic in enumerate(all_data):
-            # i 也是第几个feature
+            # f 也是第几个feature
             all_coord = dic.get("geometry").get("coordinates")
-            find_breakpoint(all_coord,offshore=offshore) #检查是否触碰边界
-            find_outliner(all_coord) #检查是否极端值
-            
+            for i,p in enumerate(all_coord):
+                #i 是第几个polygon 
+                ring = p[0] # 只取第一个ring（忽略hole）
+                if len(p) >1:
+                    print(f"Number{i} polygon in feature {f} has a hole")
+                #针对每个polygon检测(ring == polygon)
+                print(f"检测第{f}个feature,第{i}个polygon")
+                find_breakpoint(ring,offshore=offshore) #检查是否触碰边界
+                find_crosspoint(ring) #检测偏离质心大于90度
+                reconstruct_polygon(ring) #重构每个ring         
         
     elif data.get("type") == "Feature":
         all_coord = data.get("geometry").get("coordinates")
         find_breakpoint(all_coord) 
-        find_outliner(all_coord)
+        
         
             
 ## 运行main
