cd1516edbca4a0a63ac5beb39fd944f1cd3ce9c9
你的GitHub用户名yuyingxieuw <xieyuying993@gmail.com>
2026-01-15 18:36:24 -0800

    try some new ways

--- script/reshape_v3.py
+++ script/reshape_v3.py
@@ -1,51 +1,101 @@
-from spilhaus import from_lonlat_to_spilhaus_xy
-from spilhaus import make_spilhaus_xy_gridpoints
-from reshape_v2 import find_crosspoint
+
 import matplotlib.pyplot as plt
 import geopandas as gpd 
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import LineString, Polygon, MultiPolygon
 from shapely import make_valid
 import xarray as xr 
 import pandas as pd
 import numpy as np 
 import json
-import pyproj
-from reshape_v2 import find_breakpoint
+from pyproj import CRS, Transformer
+from shapely.ops import transform
+from spilhaus import from_spilhaus_xy_to_lonlat
+
+extreme = 16168767 # pyproj biggest number can be tranfered from 54099 to 4326 
+crs_54099 = CRS.from_proj4("+proj=spilhaus +lat_0=-49.56371678 +lon_0=66.94970198 +azi=40.17823482 +k_0=1.4142135623731 +rot=45 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs")
+crs_4326 = CRS.from_epsg(4326)
+tform = Transformer.from_crs(crs_54099,crs_4326, always_xy = True)
+# tform_re = Transformer.from_crs(crs_4326, crs_54099, always_xy=True)
+def tx_point(coord):
+        x,y = tform.transform(coord[0],coord[1])
+        return [x,y]
+
+n1, n2 = -extreme, extreme
+n = 200
+middle_values = np.linspace(n1, n2, n).tolist()
+
+## 构造 spilhaus north 边界线 - 两个extreme之间插点
+y = extreme
+points_n = [[x,y] for x in middle_values]
+points_wgs_north =[]
+for point in points_n:
+        coord_new = tx_point(point)
+        points_wgs_north.append(coord_new)
+# print (points_wgs)
+
+# 构造新的上north边界 
+# line_geom_north = LineString(points_wgs_north)
+# line_wgs_north = gpd.GeoSeries([line_geom_north], crs = "EPSG: 4326")
+
+## 构造 spilhaus south 边界线 - 两个extreme之间插点
+y = -extreme
+points_s = [[x,y] for x in middle_values]
+points_wgs_south =[]
+for point in points_s:
+        coord_new = tx_point(point)
+        points_wgs_south.append(coord_new)
 
 
-inpath = "/Users/xy/Documents/workspace/SpilhausPolygon/data/test.geojson"
-# test_4326 = gpd.read_file(inpath)
-# test_4326.plot(color="black", edgecolor="black")
-
-with open (inpath, "r", encoding = "utf-8") as f:
-    test = json.load(f)
-# print(test)
-poly = test.get("features")[0].get("geometry")
-ring = poly.get("coordinates")
-
-coords = ring[0][0]
-# print (coords)
-split = find_breakpoint(coords)
-print (split)
+## 构造 spilhaus west 边界线 - 两个extreme之间插点
+x = -extreme
+points_w = [[x,y] for y in middle_values]
+points_wgs_west =[]
+for point in points_w:
+        coord_new = tx_point(point)
+        points_wgs_west.append(coord_new)
 
 
+## 构造 spilhaus east 边界线 - 两个extreme之间插点
+x = extreme
+points_e = [[x,y] for y in middle_values]
+points_wgs_east =[]
+for point in points_e:
+        coord_new = tx_point(point)
+        points_wgs_east.append(coord_new)
 
 
-# china_4236 = gpd.read_file(inpath)
-# spilhaus_crs = "ESRI:54099"
-# china_54099 = china_4236.to_crs(spilhaus_crs)
-# china_valid = china_54099.make_valid()
-# print(f"Is valid: {china_valid.is_valid}")
-# china_4326.plot(color="black", edgecolor="white")
-# extreme = 11825474
-# mask_coords = [
-#     (-extreme, -extreme),
-#     (extreme, -extreme),
-#     (extreme, extreme),
-#     (-extreme, extreme)
-# ]
+## 构造geojson 
+data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+        "properties": { "boundary": "north"  },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": points_wgs_north }},
+    {
+      "type": "Feature",
+       "properties": { "boundary": "south" },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": points_wgs_south }},
+    {
+      "type": "Feature",
+       "properties": { "boundary": "west" },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": points_wgs_west }},
+    {
+      "type": "Feature",
+       "properties": { "boundary": "east" },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": points_wgs_east }}
+  ]
+}
 
-# clip_poly = Polygon(mask_coords)
-# mask_gdf = gpd.GeoDataFrame({'geometry': [clip_poly]}, crs = spilhaus_crs)
+print(data)
 
-# land_prettified = gpd.clip(china_54099, mask_gdf)
+with open("/Users/xy/Documents/workspace/SpilhausPolygon/data/boundaries.geojson", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
